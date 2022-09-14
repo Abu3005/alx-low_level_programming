71cc4f76e3c4feb0985dc0501fa36d2889647948
Adetona Mustapha <adetona3005@gmail.com>
2022-09-14 10:08:00 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,7 +7,6 @@
  * Description: prints the times table
  *
  * Return: nothing.
- *
  */
 
 void print_times_table(int n)
@@ -21,8 +20,7 @@ void print_times_table(int n)
 			for (column = 0; column <= n; column++)
 			{
 				product = row * column;
-
-
+				
 				if (column == 0)
 					_putchar('0');
 				else if (product < 10)
@@ -34,6 +32,14 @@ void print_times_table(int n)
 					_putchar(product % 10 + '0');
 				}
 				else if (product >= 10 && product < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((product / 10) % 10 + '0');
+					_putchar(product % 10 + '0');
+				}
+				else if (product > 99 && product < 1000)
 				{
 					_putchar(',');
 					_putchar(' ');
