68c7c82bd3d001162efb506c275e2e64f9e6ce11
Adetona Mustapha <adetona3005@gmail.com>
2022-09-22 06:19:32 -0700

    correction

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ * main - check the code
+ * Return: Always 0
+ */
 int main(void)
 {
 	int n;
@@ -8,7 +11,7 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	/**
+	/*
 	 * write your line of code here...
 	 * Remember:
 	 * -you are not allowed to use a
