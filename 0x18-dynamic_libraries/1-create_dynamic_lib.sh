5c5c41bda154c1615d099295d9fae716cd36bd12
Adetona Mustapha <adetona3005@gmail.com>
2023-01-19 14:50:45 +0100

    task

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-gcc *.o -fPIC -shared -o liball.so
-LD_PRELOAD=$WPD/liball.so
+gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
+gcc -shared -o liball.so *.o
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
